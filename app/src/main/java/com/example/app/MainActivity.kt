452f6180438f3b353d4e6e512f0d0ebc9e6a9657
falen <u.kerja2020@gmail.com>
2020-11-21 13:38:53 +0700

    score polos

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.app
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val spinner: Spinner = findViewById(R.id.planets_spinner)
-            // Create an ArrayAdapter using the string array and a default spinner layout
+        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
             this,
             R.array.planets_array,
@@ -23,6 +25,12 @@ class MainActivity : AppCompatActivity() {
             // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
-    }
 
+        val button = findViewById<Button>(R.id.scrore)
+        button.setOnClickListener {
+            val intent = Intent(this, scrore::class.java)
+            startActivity(intent)
+
+        }
+    }
 }
---
+++ app/src/main/java/com/example/app/scrore.kt
@@ -0,0 +1,18 @@
+package com.example.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+
+class scrore : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrore)
+
+        // Retrieves data from datasource
+        val flowerList = Datasource(this).getFlowerList()
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = FlowerAdapter(flowerList)
+    }
+}
